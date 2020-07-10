97739e46fbea71ddb4a555532e50be19141a81ad
Matt Hamlin <matthewjameshamlin@gmail.com>
2020-07-10 14:38:52 -0700

    Add types file and bump to 0.0.2

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+export interface Types {
+  string: 'string'
+  number: 'number'
+  range: 'range'
+  enum: 'enum'
+  boolean: 'bool'
+  shape: 'shape'
+  function: 'function'
+  node: 'node'
+  component: 'component'
+}
+
+export let types: Types
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,23 @@
-export let types = {
+export interface Types {
+  string: 'string'
+  number: 'number'
+  range: 'range'
+  enum: 'enum'
+  boolean: 'bool'
+  shape: 'shape'
+  function: 'function'
+  node: 'node'
+  component: 'component'
+}
+
+export let types: Types = {
   string: 'string',
   number: 'number',
   range: 'range',
   enum: 'enum',
   boolean: 'bool',
   shape: 'shape',
-  // @todo node, element, function
+  node: 'node',
+  component: 'component',
+  function: 'function',
 }
